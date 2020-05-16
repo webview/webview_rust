72356328939f6d03138c81ad06c980e731494da4
tensor-programming <tensordeveloper@gmail.com>
2020-05-16 01:12:18 -0400

    fix veersion

--- build.rs
+++ build.rs
@@ -49,7 +49,7 @@ fn main() {
         println!("cargo:rustc-link-lib=framework=WebKit");
     } else if target.contains("linux") || target.contains("bsd") {
         pkg_config::Config::new()
-            .atleast_version("3.0")
+            .atleast_version("2.8")
             .probe("webkit2gtk-4.0")
             .unwrap();
 
