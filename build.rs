709b7036bfc1dbb35efd12bb36bf6f66e0e3d28b
tensor-programming <tensordeveloper@gmail.com>
2020-05-16 01:16:19 -0400

    add webkit to include

--- build.rs
+++ build.rs
@@ -48,11 +48,14 @@ fn main() {
         println!("cargo:rustc-link-lib=framework=Cocoa");
         println!("cargo:rustc-link-lib=framework=WebKit");
     } else if target.contains("linux") || target.contains("bsd") {
-        pkg_config::Config::new()
+        let lib = pkg_config::Config::new()
             .atleast_version("2.8")
             .probe("webkit2gtk-4.0")
             .unwrap();
 
+        for path in lib.include_paths {
+            build.include(path);
+        }
         // pkg_config::Config::new()
         //     .atleast_version("3.0")
         //     .probe("gtk+-3.0")
