906caf4bb0b86e7c6eab53ff68f7b2282275cd8e
Ngo Iok Ui <wusyong9104@gmail.com>
2020-07-13 15:02:19 +0800

    fea: add builder pattern

--- examples/sample.rs
+++ examples/sample.rs
@@ -1,19 +1,24 @@
-use webview_rust_sys::{SizeHint, Webview};
+use webview_rust_sys::{SizeHint, WebviewBuilder};
 
 fn main() {
-    let mut data = Webview::create(true, None);
-    data.set_title("TEST");
-    data.set_size(1024, 768, SizeHint::NONE);
-    data.init("window.x = 42");
-    data.dispatch(|w| {
-        w.set_size(800, 600, SizeHint::MIN);
-        println!("Hello World");
-    });
-    let mut w = data.clone();
-    data.bind("xxx", move |seq, _req| {
+    let mut webview = WebviewBuilder::new()
+        .debug(true)
+        .title("TEST")
+        .width(1024)
+        .height(768)
+        .resize(SizeHint::NONE)
+        .init("window.x = 42")
+        .dispatch(|w| {
+            w.set_size(800, 600, SizeHint::MIN);
+            println!("Hello World");
+        })
+        .url("https://google.com")
+        .build();
+
+    let mut w = webview.clone();
+    webview.bind("xxx", move |seq, _req| {
         w.eval("console.log('The anwser is ' + window.x);");
         w.r#return(seq, 0, "{ result: 'We always knew it!' }");
     });
-    data.navigate("https://google.com");
-    data.run();
+    webview.run();
 }
