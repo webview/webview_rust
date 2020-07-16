0af35b80b2ccea58e7ec7ce8d83c284b625047ef
Ngo Iok Ui <wusyong9104@gmail.com>
2020-07-16 18:41:09 +0800

    Add example for WebviewMut

---
+++ examples/run_and_terminate.rs
@@ -0,0 +1,22 @@
+use std::{thread, time};
+use webview_rust_sys::{SizeHint, WebviewBuilder};
+
+fn main() {
+    let mut webview = WebviewBuilder::new()
+        .debug(true)
+        .title("TEST")
+        .width(800)
+        .height(600)
+        .resize(SizeHint::NONE)
+        .url("https://google.com")
+        .build();
+
+    let mut webview_ref = webview.as_mut();
+    
+    thread::spawn(move || {
+        thread::sleep(time::Duration::from_secs(5));
+        webview_ref.terminate().unwrap();
+    });
+
+    webview.run();
+}
