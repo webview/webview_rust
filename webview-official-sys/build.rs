f7ba383af21e13c1ac3076803a22c6a54d974894
Ngo Iok Ui (Wu Yu Wei) <wusyong9104@gmail.com>
2020-07-27 14:10:48 +0800

    chore: restructure submodule and crate, reset version and changelog (#41)
    * Fix sys crate version publish process
    
    * Reset CHANGELOG

--- webview-official-sys/build.rs
+++ webview-official-sys/build.rs
@@ -10,7 +10,7 @@ fn main() {
 
     build
         .cpp(true)
-        .include("../webview-official/webview.h")
+        .include("webview-official/webview.h")
         .flag_if_supported("/std:c++11")
         .flag_if_supported("-w");
 
@@ -23,9 +23,9 @@ fn main() {
     if target.contains("windows") {
         // build.define("UNICODE", None); // doesn't work atm.
         build
-            .file("../webview-official/webview.cc")
+            .file("webview-official/webview.cc")
             .flag_if_supported("/std:c++17");
-        build.include("../webview-official/script");
+        build.include("webview-official/script");
 
         for &lib in &["windowsapp", "user32", "oleaut32", "ole32"] {
             println!("cargo:rustc-link-lib={}", lib);
@@ -40,7 +40,7 @@ fn main() {
         // calculate full path to WebView2Loader.dll
         let mut webview2_path_buf = PathBuf::from(env::current_dir().unwrap().to_str().unwrap());
         webview2_path_buf
-            .push("../webview-official/script/Microsoft.Web.WebView2.0.9.488/build/native");
+            .push("webview-official/script/Microsoft.Web.WebView2.0.9.488/build/native");
         webview2_path_buf.push(webview2_arch);
         let webview2_dir = webview2_path_buf.as_path().to_str().unwrap();
 
@@ -66,7 +66,7 @@ fn main() {
 
         fs::copy(src_asm_buf.as_path(), target_asm_buf.as_path()).unwrap();
     } else if target.contains("apple") {
-        build.file("../webview-official/webview.cc").flag("-std=c++11");
+        build.file("webview-official/webview.cc").flag("-std=c++11");
 
         println!("cargo:rustc-link-lib=framework=Cocoa");
         println!("cargo:rustc-link-lib=framework=WebKit");
@@ -84,13 +84,13 @@ fn main() {
         //     .probe("gtk+-3.0")
         //     .unwrap();
 
-        build.file("../webview-official/webview.cc");
+        build.file("webview-official/webview.cc");
     } else {
         panic!("Unsupported platform");
     }
 
-    println!("cargo:rerun-if-changed=../webview-official/webview.h");
-    println!("cargo:rerun-if-changed=../webview-official/webview.cc");
+    println!("cargo:rerun-if-changed=webview-official/webview.h");
+    println!("cargo:rerun-if-changed=webview-official/webview.cc");
 
     build.compile("webview");
 }
